da2269398f6d2934b67186d0bee6b8fab5049d21
GitLqr <960432662@qq.com>
2017-05-24 13:16:36 +0800

    初次提交

--- README.md
+++ README.md
@@ -1,2 +1,117 @@
-# LQRNativePicSelect
-对选择系统原生图片需求做了一个工具类封装
+# 一、简述 
+
+从 7.0 开始，Android SDK 中的 StrictMode 策略禁止开发人员在应用外部公开 file:// URI。具体表现为，当我们在应用中使用包含 file:// URI 的 Intent 离开自己的应用时，程序会发生故障。本人根据网上给出的适配教程，成功在图片选择及拍照方面适配上了Android 7.0，并自己封装了一个工具类，为了今后开发更加快捷，于是本库就诞生了，如果你使用本库的话，只需少量配置并调用本库工具类中提供的3个方法，便可轻松完成拍照或图库选取图片的功能需求，且完全不需要考虑Android系统版本的问题，下面来看看如何使用本库。
+
+
+# 二、使用
+
+## 1、依赖与权限
+
+### 1）依赖
+
+	compile 'com.lqr.picselect:library:1.0.0'
+
+### 2）权限
+
+    <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
+    <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
+    <uses-permission android:name="android.permission.CAMERA"/>
+
+## 2、配置xml与清单文件 
+
+如果嫌麻烦，可以把Demo中的file\_provider\_paths.xml和清单文件中的配置复制到你的项目中。
+
+### 1）xml配置
+在res下创建一个xml文件夹，在该xml文件夹中创建一个名为file\_provider\_paths.xml的文件，内容如下：
+
+	<?xml version="1.0" encoding="utf-8"?>
+	<paths xmlns:android="http://schemas.android.com/apk/res/android">
+	    <!--路径是SD卡根目录，别名myFile-->
+	    <files-path name="myFile" path=""/>
+	    <root-path path="" name="camera_photos" />
+	</paths>
+	
+### 2）清单文件配置 
+
+打开AndroidManifest.xml文件，在Application标签中加入FileProvider声明：
+
+	<application
+        android:icon="@mipmap/ic_launcher"
+        android:theme="@style/AppTheme">
+		...
+        <provider
+            android:name="android.support.v4.content.FileProvider"
+            android:authorities="${applicationId}.fileprovider"
+            android:exported="false"
+            android:grantUriPermissions="true">
+            <meta-data
+                android:name="android.support.FILE_PROVIDER_PATHS"
+                android:resource="@xml/file_provider_paths"/>
+        </provider>
+    </application>
+
+## 3、代码调用
+
+### 1）创建LQRPhotoSelectUtils
+LQRPhotoSelectUtils提供了两个构造函数，分别是：
+
+	// 可以自定义是否开启图片剪切功能（无法设置剪切大小，默认800*480）
+	public LQRPhotoSelectUtils(Activity activity, PhotoSelectListener listener, boolean shouldCrop)
+	// 默认开启图片剪切功能，可以指定剪切大小
+	public LQRPhotoSelectUtils(Activity activity, PhotoSelectListener listener, int aspectX, int aspectY, int outputX, int outputY) 
+
+>这里有两个要说明的：
+
+>1. 部分国产机无法使用图片剪切功能，例如华为TAG-AL00就阉割了图片剪切，但本人亲测小米3就可以进行图片剪切，这是系统优化差异造成的，不是本库的问题！！！
+1. 此外，就算可以进行图片剪切，也不一定可以固定图片大小，亲测小米3与坚果pro无法固定剪切图片的大小，又因为手头上没有“谷歌亲儿子”，所以没法断定是不是本库代码问题，故在此，本人建议使用第一个构造方法。
+
+使用第一个构造方法创建LQRPhotoSelectUtils对象示例如下：
+
+	// 创建LQRPhotoSelectUtils（一个Activity对应一个LQRPhotoSelectUtils）
+    mLqrPhotoSelectUtils = new LQRPhotoSelectUtils(this, new LQRPhotoSelectUtils.PhotoSelectListener() {
+        @Override
+        public void onFinish(File outputFile, Uri outputUri) {
+            // 当拍照或从图库选取图片成功后回调
+            mTvPath.setText(outputFile.getAbsolutePath());
+            mTvUri.setText(outputUri.toString());
+            Glide.with(MainActivity.this).load(outputUri).into(mIvPic);
+        }
+    }, true);
+
+### 2）关联onActivityResult()
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 在Activity中的onActivityResult()方法里与LQRPhotoSelectUtils关联
+        mLqrPhotoSelectUtils.attachToActivityForResult(requestCode, resultCode, data);
+    }
+
+### 3）调用拍照或从图库选取图片方法
+
+	// 调用相机进行拍照
+	mLqrPhotoSelectUtils.takePhoto();
+	// 调用图库选取图片
+	mLqrPhotoSelectUtils.selectPhoto();
+
+
+### 4）其他设置
+
+	// 设置FileProvider的主机名
+	mLqrPhotoSelectUtils.setAuthorities();
+	// 修改图片的存储路径
+    mLqrPhotoSelectUtils.setImgPath();
+
+>注意：
+>
+如果你app的build.gradle中，defaultConfig{}的applicationId对应的值不是应用包名的话就需要调用setAuthorities()方法;也设置FileProvider的主机名，这是为了适配Android 7.0。取值是applicationId对应的值+".fileprovider"。若applicationId对应的值是应用包名的话则不需理会。
+
+## 4、效果
+
+如有问题请参考本库的Demo，相信问题并不大，下面看下Demo的运行效果：
+
+![](screenhots/1.gif)
+
+
+
+
---
+++ app/src/main/java/com/lqr/lqrnativepicselect/MainActivity.java
@@ -0,0 +1,121 @@
+package com.lqr.lqrnativepicselect;
+
+import android.Manifest;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.lqr.picselect.LQRPhotoSelectUtils;
+
+import java.io.File;
+
+import kr.co.namee.permissiongen.PermissionFail;
+import kr.co.namee.permissiongen.PermissionGen;
+import kr.co.namee.permissiongen.PermissionSuccess;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnTakePhoto;
+    private Button mBtnSelectPhoto;
+    private TextView mTvPath;
+    private TextView mTvUri;
+    private LQRPhotoSelectUtils mLqrPhotoSelectUtils;
+    private ImageView mIvPic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBtnTakePhoto = (Button) findViewById(R.id.btnTakePhoto);
+        mBtnSelectPhoto = (Button) findViewById(R.id.btnSelectPhoto);
+        mTvPath = (TextView) findViewById(R.id.tvPath);
+        mTvUri = (TextView) findViewById(R.id.tvUri);
+        mIvPic = (ImageView) findViewById(R.id.ivPic);
+
+        init();
+        initListener();
+    }
+
+    private void init() {
+        // 1、创建LQRPhotoSelectUtils（一个Activity对应一个LQRPhotoSelectUtils）
+        mLqrPhotoSelectUtils = new LQRPhotoSelectUtils(this, new LQRPhotoSelectUtils.PhotoSelectListener() {
+            @Override
+            public void onFinish(File outputFile, Uri outputUri) {
+                // 4、当拍照或从图库选取图片成功后回调
+                mTvPath.setText(outputFile.getAbsolutePath());
+                mTvUri.setText(outputUri.toString());
+                Glide.with(MainActivity.this).load(outputUri).into(mIvPic);
+            }
+        }, true);
+
+//        mLqrPhotoSelectUtils.setAuthorities("com.lqr.lqrnativepicselect.fileprovider");
+//        mLqrPhotoSelectUtils.setImgPath(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + String.valueOf(System.currentTimeMillis()) + ".jpg");
+    }
+
+    private void initListener() {
+        mBtnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 3、调用拍照方法
+                PermissionGen.with(MainActivity.this)
+                        .addRequestCode(LQRPhotoSelectUtils.REQ_TAKE_PHOTO)
+                        .permissions(Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                Manifest.permission.CAMERA
+                        ).request();
+            }
+        });
+
+        mBtnSelectPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 3、调用从图库选取图片方法
+                PermissionGen.needPermission(MainActivity.this,
+                        LQRPhotoSelectUtils.REQ_SELECT_PHOTO,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
+                                Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                );
+            }
+        });
+    }
+
+    @PermissionSuccess(requestCode = LQRPhotoSelectUtils.REQ_TAKE_PHOTO)
+    private void takePhoto() {
+        mLqrPhotoSelectUtils.takePhoto();
+    }
+
+    @PermissionSuccess(requestCode = LQRPhotoSelectUtils.REQ_SELECT_PHOTO)
+    private void selectPhoto() {
+        mLqrPhotoSelectUtils.selectPhoto();
+    }
+
+    @PermissionFail(requestCode = LQRPhotoSelectUtils.REQ_TAKE_PHOTO)
+    private void showTip1() {
+        Toast.makeText(getApplicationContext(), "不给我权限是吧，那就别玩了", Toast.LENGTH_SHORT).show();
+    }
+
+    @PermissionFail(requestCode = LQRPhotoSelectUtils.REQ_SELECT_PHOTO)
+    private void showTip2() {
+        Toast.makeText(getApplicationContext(), "不给我权限是吧，那就别玩了", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        PermissionGen.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 2、在Activity中的onActivityResult()方法里与LQRPhotoSelectUtils关联
+        mLqrPhotoSelectUtils.attachToActivityForResult(requestCode, resultCode, data);
+    }
+}
